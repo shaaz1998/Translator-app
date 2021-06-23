483b1cb8c054e84a0a9333aa6f739ae24a749f95
shaaz1998 <kmdshaaz8@gmail.com>
2021-06-23 14:11:56 +0530

    update

---
+++ gui.js
@@ -0,0 +1,16 @@
+var txtArea = document.querySelector("#txtArea")
+var button = document.querySelector("#btnClick")
+var output = document.querySelector("#output")
+
+
+
+ 
+
+button.addEventListener("click", function(){
+    // console.log("heyyy "+ txtArea.value )
+    console.log( output.innerText = txtArea.value)
+
+
+    
+    
+})
